05bf0ab5ba486866fc462f3bd2127f2641ff6eca
Artem <temaspsxxx@gmail.com>
2020-10-07 18:25:42 +0300

    feat: add configs and components

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist/'),
+    filename: 'index.js',
+    publicPath: 'dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
