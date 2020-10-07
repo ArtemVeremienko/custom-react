799d7c86bcc64de5cc645952a100906e3e620d10
Artem <temaspsxxx@gmail.com>
2020-10-07 23:50:56 +0300

    feat: add onchange imitation

--- src/app.js
+++ src/app.js
@@ -4,30 +4,46 @@ import PropTypes from 'prop-types'
 const App = ({ min, max }) => {
 
   const [current, setCurrent] = useState(min)
+  const [inputValue, setInputValue] = useState(min)
 
   const decrease = () => {
     const nextCurrent = current - 1
-    if (nextCurrent < min) return
-    setCurrent(nextCurrent)
+    set(nextCurrent)
   }
 
   const increase = () => {
     const nextCurrent = current + 1
-    if (nextCurrent > max) return
-    setCurrent(nextCurrent)
+    set(nextCurrent)
   }
 
-  const handlerInput = (e) => {
-    let value = parseInt(e.target.value)
-    if (isNaN(value)) return
-    value = Math.max(min, Math.min(max, value))
+  const handleInput = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const set = (val) => {
+    const value = Math.max(min, Math.min(max, val))
     setCurrent(value)
+    setInputValue(value)
+  }
+
+  const checkValue = () => {
+    let value = parseInt(inputValue)
+    set(isNaN(value) ? min : value)
+  }
+
+  const checkEnterPress = ({ key }) => {
+    if (key === 'Enter') checkValue()
   }
 
   return (
     <div>
       <button type="button" onClick={decrease}>-</button>
-      <input type="text" value={current} onChange={handlerInput} />
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInput}
+        onBlur={checkValue}
+        onKeyUp={checkEnterPress} />
       <button type="button" onClick={increase}>+</button>
     </div >
   )
