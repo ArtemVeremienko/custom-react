496e45050e66cba433e9f2d636eb573f35f6af57
Artem <temaspsxxx@gmail.com>
2020-10-12 20:51:18 +0300

    feat: create result page

--- src/pages/result/result.js
+++ src/pages/result/result.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
 
-import { observer } from 'mobx-react';
-
 import cartModel from '~s/cart.js';
 import orderModel from '~s/order.js';
 
-export default observer(() => {
-  const { values } = orderModel;
+import { urlBuilder } from '~/routes'
+import { Link } from 'react-router-dom'
+
+export default () => {
+  const postsTmp = [1, 2, 3]
+  const links = postsTmp.map(post =>
+    <div key={post}>
+      <Link to={urlBuilder('blogPost', { some: post })}>
+        Post {post}
+      </Link>
+    </div>
+  )
+  const { data } = orderModel;
 
   return (
     <Jumbotron className="text-center">
-      <h2>Congratulations {values.name} with your purchase!</h2>
+      <h2>Congratulations {data.name} with your purchase!</h2>
       <p>Total amount: <strong>{cartModel.total}&#8381;</strong></p>
+      <div>
+        {links}
+      </div>
     </Jumbotron>
   )
-});
+};
