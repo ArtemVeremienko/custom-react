ea8147fc5449ef46db0d9b8beee6549ffa2d6d60
Artem <temaspsxxx@gmail.com>
2020-10-14 23:59:27 +0300

    refactor: create navbar element

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Container, Row, Col, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
-import { observer } from 'mobx-react';
+import { observer, Provider } from 'mobx-react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import routes from '~/routes'
-import { routesMap } from '~/routes'
+import routes, { routesMap } from '~/routes'
 import { Link, NavLink } from 'react-router-dom'
+
+import stores from '~s/rootstore'
 
 @observer class App extends React.Component {
   render() {
+
+    const navbar = (
+      <Navbar bg="primary" variant="dark">
+        <Link to={routesMap.home} className="navbar-brand">Home</Link>
+        <Nav className="mr-auto">
+          <NavLink to={routesMap.cart} className="nav-link">Cart</NavLink>
+          <NavLink to={routesMap.order} className="nav-link">Order</NavLink>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light">Search</Button>
+        </Form>
+      </Navbar>
+    )
 
     const routesComponents = routes.map(({ url, component, exact }) =>
       <Route path={url} component={component}
@@ -15,31 +30,23 @@ import { Link, NavLink } from 'react-router-dom'
     )
 
     return (
-      <Router>
-        <Container>
-          <Row>
-            <Col>
-              <Navbar bg="primary" variant="dark">
-                <Link to={routesMap.home} className="navbar-brand">Home</Link>
-                <Nav className="mr-auto">
-                  <NavLink to={routesMap.cart} className="nav-link">Cart</NavLink>
-                  <NavLink to={routesMap.order} className="nav-link">Order</NavLink>
-                </Nav>
-                <Form inline>
-                  <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                  <Button variant="outline-light">Search</Button>
-                </Form>
-              </Navbar>
-            </Col>
-          </Row>
+      <Provider stores={stores}>
+        <Router>
+          <Container>
+            <Row>
+              <Col>
+                {navbar}
+              </Col>
+            </Row>
 
-          <Row>
-            <Switch>
-              {routesComponents}
-            </Switch>
-          </Row>
-        </Container>
-      </Router>
+            <Row>
+              <Switch>
+                {routesComponents}
+              </Switch>
+            </Row>
+          </Container>
+        </Router>
+      </Provider>
     )
   }
 }
