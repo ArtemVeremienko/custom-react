7cbc0507622ba931eb49b876d40a0b86d38ad336
Artem <temaspsxxx@gmail.com>
2020-10-14 23:10:59 +0300

    feat: add products store

---
+++ src/store/products.js
@@ -0,0 +1,61 @@
+import { makeObservable, observable, computed, action } from 'mobx'
+
+export default class {
+  constructor(rootStore) {
+    makeObservable(this)
+    this.rootStore = rootStore
+  }
+
+  @observable items = getProducts()
+
+  @computed get productsMap() {
+    return this.items.reduce((obj, { id }, i) => {
+      obj[id] = i
+      return obj
+    }, {})
+  }
+
+  getBy(id) {
+    const index = this.productsMap[id]
+    if (index === undefined) return null
+    return this.items[index]
+  }
+}
+
+
+
+
+
+// server api
+function getProducts() {
+  return [
+    {
+      id: 100,
+      title: 'Ipnone 200',
+      price: 12000,
+      rest: 10,
+      current: 1
+    },
+    {
+      id: 101,
+      title: 'Samsung AAZ8',
+      price: 22000,
+      rest: 5,
+      current: 1
+    },
+    {
+      id: 103,
+      title: 'Nokia 3310',
+      price: 5000,
+      rest: 2,
+      current: 1
+    },
+    {
+      id: 105,
+      title: 'Huawei ZZ',
+      price: 15000,
+      rest: 8,
+      current: 1
+    }
+  ];
+}
