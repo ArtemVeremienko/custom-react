65ef7cb4393dc47f27f3d2a5b30f701c7c996251
Artem <temaspsxxx@gmail.com>
2020-10-15 11:30:56 +0300

    fix: responsive

--- src/components/errors/404/404.js
+++ src/components/errors/404/404.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { routesMap } from '~/routes'
+import { Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export default () => {
   return (
-    <>
+    <Col className="text-sm-center">
       <h1>Error 404, page not found</h1>
       <hr />
       <div className="alert alert-warning">
@@ -14,6 +15,6 @@ export default () => {
           </Link>
         </p>
       </div>
-    </>
+    </Col>
   )
 }
