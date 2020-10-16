fd4091b87908bbafa029b73215dd374bd4bddf6c
Artem <temaspsxxx@gmail.com>
2020-10-16 12:41:12 +0300

    feat: add api to rootstore

--- src/store/rootstore.js
+++ src/store/rootstore.js
@@ -2,8 +2,18 @@ import cartStore from './cart'
 import productsStore from './products'
 import orderStore from './order'
 
+import * as products from '~/api/products'
+import * as cart from '~/api/cart'
+
+console.log(products.all())
+
 class RootStore {
   constructor() {
+    this.api = {
+      products,
+      cart,
+    }
+
     this.cart = new cartStore(this)
     this.products = new productsStore(this)
     this.order = new orderStore(this)
